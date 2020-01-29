71a70e46d079e6adc2dab4814a3f4177fd339d58
Daniel Ferreira Lopes <daniel.ferreira-lopes@scrt.ch>
2020-01-29 15:29:27 +0100

    Add mirrors setup for archlinux

---
+++ archlinux/conf/0_setupmirrors.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pacman -S --noconfirm pacman-contrib
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+curl -s "https://www.archlinux.org/mirrorlist/?country=CH&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
---
+++ archlinux/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/df3l0p/setupfiles
+chmod +x setupfiles/archlinux/conf/0_setupmirrors.sh
+./setupfiles/archlinux/conf/0_setupmirrors.sh
